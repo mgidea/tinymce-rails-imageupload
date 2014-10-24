42e1c97fcfebe25a5bc778ec4f49e356778bc94f
Tom Corley <tom.corley@goodmeasures.com>
2014-10-24 14:57:14 -0400

    create hidden field to store checkbox submit

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -61,6 +61,8 @@
         form.appendChild(createElement('input', {type: "hidden", name: "utf8", value: "✓"}));
         form.appendChild(createElement('input', {type: 'hidden', name: 'authenticity_token', value: getMetaContents('csrf-token')}));
         form.appendChild(createElement('input', {type: 'hidden', name: 'hint', value: ed.getParam("uploadimage_hint", "")}));
+        form.appendChild(createElement('input', {type: 'hidden', name: "store_document", id: "store_document", value: 'false'}));
+
         var el = win.getEl();
         var body = document.getElementById(el.id + "-body");
 
